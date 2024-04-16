1b08fadce2b1f3496003cd19aaec1fe0a0880d84
Yung Yau <yylee2998@gmail.com>
2024-04-16 22:37:08 +0800

    Initialise Branch

--- GenshinHelper_Main.py
+++ GenshinHelper_Main.py
@@ -1,5 +1,25 @@
 #This is a Project of TT4L-06 CSP1123
 
 import tkinter as tk 
+import csv
 
-#Main Menu
+###Main Menu
+
+###Character Database
+CharacterNames = ['Albedo',]
+
+#Character Quality
+Character_5stars = ['Albedo',]
+Character_4stars = 
+
+#Character Search Features
+rows = []
+
+with open('Character.csv','r') as file:
+    csvreader = csv.reader(file)
+    header = next(csvreader)
+    for row in csvreader:
+        rows.append(row)
+
+print(header)
+print(rows)
