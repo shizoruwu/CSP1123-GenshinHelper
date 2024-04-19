019896be99a1d4836ec62ce8ef2243ee82096d7b
Yung Yau <yylee2998@gmail.com>
2024-04-19 15:03:52 +0800

    ADDED Search Function to Remove Unrelated Object

--- search.py
+++ search.py
@@ -4,6 +4,7 @@ import sqlite3
 import tkinter as tk
 from tkinter import ttk
 
+#Character List Names ( DATA )
 CharaLst = ['Albedo','Alhaitam','Aloy','Amber','Arataki Itto'
             ,'Baizhu','Barbara','Beidou','Bennett','Candace'
             ,'Charlotte','Chevreuse','Chiori','Chongyun','Collei'
@@ -19,9 +20,7 @@ CharaLst = ['Albedo','Alhaitam','Aloy','Amber','Arataki Itto'
             ,'Wanderer','Wriothesley','Xiangling','Xianyun','Xiao','Xingqiu'
             ,'Xinyan','Yae Miko','Yanfei','Yaoyao','Yelan','Yoimiya','Yun Jin','Zhongli']
 
-
-
-class search(tk.Tk):
+class charactersearch(tk.Tk):
     def __init__(self):
         
       tk.Tk.__init__(self)
@@ -36,6 +35,7 @@ class search(tk.Tk):
       font = ("Times New Roman", 12)).grid(column = 0, row = 2, padx = 0, pady = 5) 
         
       textn = tk.StringVar
+      global CharaChosen
       CharaChosen = ttk.Combobox(self,font = ("Times New Roman", 12),values=CharaLst,width=30,textvariable=textn)
       CharaChosen.grid(column = 1, row = 2 , padx = 5, pady = 5 )
       CharaChosen.current()
@@ -44,7 +44,23 @@ class search(tk.Tk):
       EnterButton = ttk.Button(self, text="ENTER", width = 8)
       EnterButton.grid(column = 2, row = 2, padx = 18, pady = 5 )
 
+      CharaChosen.bind('<KeyRelease>',searchs)
+
+#Search Function to Remove Unrelated Object
+def searchs(self):
+  searchvalue = self.widget.get()
+  if searchvalue == '':
+    CharaChosen['values'] = CharaLst
+
+  else:
+    data = []
+    for item in CharaLst:
+      if searchvalue.lower() in item.lower():
+        data.append(item)
     
+    CharaChosen['values'] = data
+        
+###UNUSED ATM Connect Data from SQLite3
 def fetchname():
   conn = sqlite3.connect('genshindata.db')
   cur = conn.cursor()
@@ -55,5 +71,5 @@ def fetchname():
   global namelist
   namelist = [list(row) for row in (namerows)]
 
-CurrentScreen = search()
+CurrentScreen = charactersearch()
 CurrentScreen.mainloop()
