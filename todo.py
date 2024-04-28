373cecdc4d64334feaf8a05c51639c8d7365931e
Jiahe <1221107110@soffice.mmu.edu.my>
2024-04-28 18:43:38 +0800

    Added a add task function.

--- todo.py
+++ todo.py
@@ -7,15 +7,15 @@ import sqlite3
 # Create a window
 root = Tk()
 root.title("To-Do List")
-root.geometry("800x400")
+root.geometry("800x400+100+100") # (w, h, x, y)
 
 # Create a frame inside the window
 frame = ttk.Frame(root, borderwidth=10)
 frame.grid(column=0, row=0) # Render
 
 # Create Label
-label = ttk.Label(frame, text="To-Do List", font=("TkDefaultFont", 12))
-label.grid(column=1, row=0, padx=(20, 5), pady=10) # Render
+label = ttk.Label(frame, text="To-Do List", font=("Georgia", 15))
+label.grid(column=1, row=0, padx=(20, 5), pady=10, sticky=W) # Render
 
 # Connect db
 DBconnection = sqlite3.connect("tasks.db")
@@ -36,7 +36,7 @@ def db2dict():
     for data in values:
         # print(data[1])
         tasks[data[0]] = eval(data[1]) # Add items to dict
-    print(f"dict(39) -> {tasks}")
+    print(f"dict(db2dict) -> {tasks}")
 
 
 
@@ -44,8 +44,22 @@ def db2dict():
 entry = ttk.Entry(frame)
 entry.grid(column=2, row=0, padx=(50, 5))
 
+def add_task():
+    # print(entry.get())
+    text = entry.get()
+    if text == '':
+        print("Error - Blank")
+    elif text not in tasks:
+        DBcursor.execute(f"INSERT INTO tasks VALUES('{text}', 'False')")
+        print("Task added.")
+    elif text in tasks:
+        DBcursor.execute(f"INSERT INTO tasks VALUES('{text} (1)', 'False')")
+        print("Task added.")
+    DBconnection.commit() # Save added task to db
+    
+
 # Create "add" task button
-add = ttk.Button(frame, text="add", width=5)
+add = ttk.Button(frame, text="Add", width=5, command=add_task)
 add.grid(column=3, row=0, pady=5, sticky=W)
 
 # Create multiple checkbox
@@ -60,12 +74,11 @@ def create_checkbox():
         tasks[task] = var
         row_init += 1
     
-    print(f"\ndict(63) -> {tasks}")
+    print(f"\ndict(create_checkbox) -> {tasks}")
 
 # print(add.configure())
 db2dict()
 create_checkbox()
-# for k, v in tasks.items():
-#     print(v.get())
+
 
 root.mainloop()
