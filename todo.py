a148fd964bf3b5486be315d58fc55df0bff4dc13
Jiahe <1221107110@soffice.mmu.edu.my>
2024-04-29 20:24:14 +0800

    Added a delete function.

--- todo.py
+++ todo.py
@@ -2,6 +2,7 @@
 
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 import sqlite3
 
 # Create a window
@@ -34,13 +35,10 @@ values = DBcursor.execute("SELECT * FROM tasks") # Read
 values = values.fetchall() # Get
 
 # Add task and status to dict
-def db2dict():
-    for data in values:
-        # print(data[1])
-        tasks[data[0]] = eval(data[1]) # Add items to dict
-    print(f"dict(db2dict) -> {tasks}")
-
-
+for data in values:
+    # print(data[1])
+    tasks[data[0]] = eval(data[1]) # Add items to dict
+print(f"dict -> {tasks}")
 
 # Create entry
 entry = ttk.Entry(frame)
@@ -96,8 +94,47 @@ refreshb = ttk.Button(frame, text="Refresh", width=9, command=refresh)
 refreshb.grid(column=4, row=0, sticky=W, padx=(5, 0))
 
 # print(add.configure())
-db2dict()
 create_checkbox()
+
+
+# Delete function
+def delete():
+    # Create another window
+    delete_window = Toplevel()
+    delete_window.title("Delete task")
+    delete_window.geometry("150x250+550+150")
+    
+    frame2 = ttk.Frame(delete_window, padding=10)
+    frame2.grid(column=0, row=0, padx=5, pady=5)
+    label2 = ttk.Label(frame, text="Delete Task")
+    # Create listbox
+    listbox = Listbox(frame2, selectmode=EXTENDED)
+    listbox.grid(column=1, row=1)
+    # Insert values to the listbox
+    for task in tasks:
+        listbox.insert(END, task)
+
+    def delete_func():
+        confirm = messagebox.askyesno(title="Delete Task", message="Are you sure you want to "
+                                "delete this/these task?", icon="warning")
+        if confirm:
+            for selected_listbox_index in listbox.curselection():
+                # print(listbox.get(selected_listbox_index))
+                DBcursor.execute(f"DELETE FROM tasks WHERE Task = '{listbox.get(selected_listbox_index)}'")
+                tasks.pop(f"{listbox.get(selected_listbox_index)}")
+        else:
+            delete_window.destroy()
+        DBconnection.commit()
+        delete_window.destroy()
+        for raw_task in raw_tasks:
+            raw_task.destroy()
+        create_checkbox(frame)
+
+    button = ttk.Button(frame2, text="Delete", width=8, command=delete_func)
+    button.grid(column=1, row=2, pady=(10, 0))
+
+delete_button = ttk.Button(frame, text="Delete", width=8, command=delete)
+delete_button.grid(column=5, row=0, sticky=W, padx=(5,0))
 
 
 root.mainloop()
