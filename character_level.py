dbb06dc8ff5269041559ddd6ed2eff4426010c05
Ng Bai Keong <keong5800@gmail.com>
2024-05-09 13:37:47 +0800

    initialise repository

---
+++ character_level.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import *
+from ctypes import windll
+
+windll.shcore.SetProcessDpiAwareness(1)
+
+#character calculator features
+root = tk.Tk()
+root.geometry("1200x650")
+root.title("Character Level Calculator")
+
+#grid
+
+
+
+#labels
+
+
+
+root.mainloop()
