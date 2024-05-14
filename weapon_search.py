17b9c46c48e3f861acfc80e294ec8009621e85ae
Yung Yau <yylee2998@gmail.com>
2024-05-14 13:58:29 +0800

    Made the Filter Checkbox Function Working + Updated All Claymore Info IN DB

--- weapon_search.py
+++ weapon_search.py
@@ -33,7 +33,7 @@ class weaponsearch(tk.Tk):
     self.weaponimage.grid(columnspan=2,column=0,row=1)
 
     #Sets the Default Value of Checkbox to CHECKED
-    self.swordbutton_value = tk.IntVar(value = 1)
+    self.swordbutton_value = tk.BooleanVar(value = 1)
     self.claymorebutton_value = tk.BooleanVar(value = 1)
     self.polearmbutton_value = tk.BooleanVar(value = 1)
     self.catalystbutton_value = tk.BooleanVar(value = 1)
@@ -43,19 +43,19 @@ class weaponsearch(tk.Tk):
     chooseweapon.grid(column=0, row=0, padx = 10,pady = 5)
 
     #Checkbox for WEAPON TYPES
-    swordtype = ttk.Checkbutton(weaponframe,text = 'Sword',variable=self.swordbutton_value)
+    swordtype = ttk.Checkbutton(weaponframe,text = 'Sword',variable=self.swordbutton_value , command=self.add_images_filtered)
     swordtype.grid(column=0,row=1,sticky = 'w',padx = 10)
 
-    claymoretype = ttk.Checkbutton(weaponframe,text = 'Claymore',variable=self.claymorebutton_value)
+    claymoretype = ttk.Checkbutton(weaponframe,text = 'Claymore',variable=self.claymorebutton_value,command=self.add_images_filtered)
     claymoretype.grid(column=0,row=2,sticky = 'w',padx = 10)
 
-    polearmtype = ttk.Checkbutton(weaponframe,text = 'Polearm',variable=self.polearmbutton_value)
+    polearmtype = ttk.Checkbutton(weaponframe,text = 'Polearm',variable=self.polearmbutton_value,command=self.add_images_filtered)
     polearmtype.grid(column=0,row=3,sticky = 'w',padx = 10)
 
-    catalysttype = ttk.Checkbutton(weaponframe,text = 'Catalyst',variable=self.catalystbutton_value)
+    catalysttype = ttk.Checkbutton(weaponframe,text = 'Catalyst',variable=self.catalystbutton_value,command=self.add_images_filtered)
     catalysttype.grid(column=0,row=4,sticky = 'w',padx = 10)
 
-    bowtype = ttk.Checkbutton(weaponframe,text = 'Bow',variable=self.bowbutton_value)
+    bowtype = ttk.Checkbutton(weaponframe,text = 'Bow',variable=self.bowbutton_value,command=self.add_images_filtered)
     bowtype.grid(column=0,row=5,sticky = 'w',padx = 10)
 
     #Create a scrollable frame
@@ -72,7 +72,7 @@ class weaponsearch(tk.Tk):
 
     self.scrollbar.config(command=self.canvas.yview)
 
-    #Create a frame inside the canvas to hold the images
+    #Create a frame inside the canvas
     self.image_frame = ttk.Frame(self.canvas)
     self.canvas.create_window((0, 0), window=self.image_frame, anchor='nw')
 
@@ -91,21 +91,21 @@ class weaponsearch(tk.Tk):
 
   #Add image Functions
   def add_images(self):
-    for i, name in enumerate(imagenamelist):
+    for i, name in enumerate(imagenamelist_sort):
       row = i // 7  # 7 images per row
       if row != 0:
         row = row*2
       col = i % 7
-      weaponname = namelist[i]
+      weaponname = namelist_sort[i]
       currentname = name
 
       label = ttk.Label(self.image_frame,width = 160,justify='center')
-      label.grid(row=row, column=col, padx=12, pady=10,sticky='n')
+      label.grid(row=row, column=col, padx=10, pady=10,sticky='n')
       label.bind("<Button-1>",lambda event, clickedname = weaponname: self.ImageClicked(event,clickedname))
 
       row = row+1
       label2 = ttk.Label(self.image_frame,justify='center',wraplength = 160,text = weaponname)
-      label2.grid(row=row, column=col, padx=12,sticky = 'n')
+      label2.grid(row=row, column=col, padx=10,sticky = 'n')
 
       #Insert Image of Weapon
       image_path = f"Genshin_Weapon_Image/Weapon_{currentname}.png"
@@ -124,15 +124,22 @@ class weaponsearch(tk.Tk):
     cur = conn.cursor()
     cur.execute("SELECT ImgName FROM WeaponData")
     namerows = cur.fetchall()
-    global imagenamelist
+    global imagenamelist , imagenamelist_sort
     imagenamelist = [row[0] for row in namerows]
-    imagenamelist.sort()
+    imagenamelist_sort = [row[0] for row in namerows]
+    imagenamelist_sort.sort()
 
     cur.execute("SELECT Name FROM WeaponData")
     namerows = cur.fetchall()
-    global namelist
+    global namelist , namelist_sort
     namelist = [row[0] for row in namerows]
-    namelist.sort()
+    namelist_sort = [row[0] for row in namerows]
+    namelist_sort.sort()
+
+    cur.execute("SELECT Type FROM WeaponData")
+    namerows = cur.fetchall()
+    global weapontype
+    weapontype = [row[0] for row in namerows]
 
     conn.close()
 
@@ -144,10 +151,10 @@ class weaponsearch(tk.Tk):
     else:
       self.show_MenuFrame()
 
-  #Show Menu Fram Func
+  #Show Menu Frame Func
   def show_MenuFrame(self):
     self.current_frame = self.MenuFrame
-    self.MenuFrame.grid()
+    self.MenuFrame.grid(padx=20, pady=20)
     self.InfoFrame.grid_forget()
 
   #Show Info Frame Func
@@ -159,9 +166,71 @@ class weaponsearch(tk.Tk):
   #Switching between frames
   def switch_frames(self):
     if self.current_frame == self.MenuFrame:
-        self.show_InfoFrame()
+      self.show_InfoFrame()
     else:
-        self.show_MenuFrame()
+      self.show_MenuFrame()
+
+  def add_images_filtered(self):
+    #Destroy Current Frame
+    self.image_frame.destroy()
+    
+    #Create another empty frame
+    self.image_frame = ttk.Frame(self.canvas)
+    self.canvas.create_window((0, 0), window=self.image_frame, anchor='nw')
+
+    #Determined Filtered Weapon Types
+    selected_types = []
+    if self.swordbutton_value.get():
+      selected_types.append("Sword")
+    if self.claymorebutton_value.get():
+      selected_types.append("Claymore")
+    if self.polearmbutton_value.get():
+      selected_types.append("Polearm")
+    if self.catalystbutton_value.get():
+      selected_types.append("Catalyst")
+    if self.bowbutton_value.get():
+      selected_types.append("Bow")
+
+    filtered_name = []
+    filtered_imgname = []
+
+    #Sort Out Filtered Names
+    for i, name in enumerate(imagenamelist):
+      weaponname = namelist[i]
+        
+      currentweapontype = weapontype[i]
+
+      if currentweapontype in selected_types:
+        filtered_name.append(weaponname)
+        filtered_imgname.append(name)
+
+      filtered_name.sort()
+      filtered_imgname.sort()
+
+    #Display New List of Data
+    for i, name in enumerate(filtered_imgname):
+      row = i // 7  # 7 images per row
+      if row != 0:
+        row = row*2
+      col = i % 7
+      weaponname = filtered_name[i]
+      currentname = name
+
+      label = ttk.Label(self.image_frame,width = 160,justify='center')
+      label.grid(row=row, column=col, padx=10, pady=10,sticky='n')
+      label.bind("<Button-1>",lambda event, clickedname = weaponname: self.ImageClicked(event,clickedname))
+
+      row = row+1
+      label2 = ttk.Label(self.image_frame,justify='center',wraplength = 160,text = weaponname)
+      label2.grid(row=row, column=col, padx=10,sticky = 'n')
+
+      #Insert Image of Weapon
+      image_path = f"Genshin_Weapon_Image/Weapon_{currentname}.png"
+      image = Image.open(image_path)
+      resized_image = image.resize((128, 128))
+      charphoto = ImageTk.PhotoImage(resized_image)
+      label.config(image=charphoto)
+      label.image = charphoto
 
 CurrentScreen = weaponsearch()
 CurrentScreen.mainloop()
