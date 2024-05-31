bd7d27eff875254f84df55a79a305e331e36042e
Jiahe <1221107110@soffice.mmu.edu.my>
2024-05-31 13:47:09 +0800

    Completed sidebar functions.

--- GenshinHelper_Main.py
+++ GenshinHelper_Main.py
@@ -4,10 +4,11 @@ import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from ctypes import windll
 from PIL import Image, ImageTk
+import os
 
 import todo
 
-
+windll.shcore.SetProcessDpiAwareness(1)
 root = ttk.Window(themename="light_4")
 root.title("Genshin Helper")
 root.geometry("900x600+100+100")
@@ -20,71 +21,170 @@ s.configure('fontt.TLabel', font=("Georgia", 15))
 s.configure('grey.TFrame', background="grey")
 
 topFrame = ttk.LabelFrame(root)
-topFrame.grid(column=1, row=1, sticky=W, padx=5, columnspan=2)
-# topFrame.pack(fill=X)
-# ttk.Separator(root).grid(column=1, row=2, columnspan=6, pady=5, sticky=EW, ipadx=350)
-# ttk.Separator(root).place(x=10, y=60, relwidth=1)
+topFrame.grid(column=1, row=1, sticky=W, padx=10, columnspan=2)
+title = ttk.Label(topFrame, text=" Genshin Helper", style="font.TLabel", bootstyle="primary")
 sideBarFrame = ttk.Frame(root)
 sideBarFrame.grid(column=1, row=2, sticky=NW, pady=5, ipady=500, ipadx=5)
-# sideBarFrame.columnconfigure(1, weight=1)
-# content = ttk.LabelFrame(root, text="testing")
-# content.grid(column=2, row=3)
+
 root.update()
 sideBarFrame.update()
-print(sideBarFrame.winfo_width())
-title = ttk.Label(topFrame, text=" Genshin Helper", style="font.TLabel", bootstyle="primary")
 
 
-# setting = ttk.Button(topFrame, text="Setting")
-# setting.grid(column=1, row=0, sticky=E, padx=(0, (root.winfo_width() - 350)), pady=(0, 10))
+## Features
+characterSearch = ttk.Label(root, text="Character")
+characterSearch.grid(column=2, row=2, sticky=N)
 
-label1 = ttk.Label(root, text="Feat 1 Test")
-label1.grid(column=2, row=3)
+CharacterLevelCalc = ttk.Label(root, text="Character Level Calculator")
+CharacterLevelCalc.grid(column=2, row=2, sticky=N)
+CharacterLevelCalc.grid_remove()
 
-labe12 = ttk.Label(root, text="Feat 2 Test")
-labe12.grid(column=2, row=3)
-labe12.grid_remove()
+weapon = ttk.Label(root, text="Weapon")
+weapon.grid(column=2, row=2, sticky=N)
+weapon.grid_remove()
 
-todoFrame = todo.ToDoAppFrame(root)
-todoFrame.grid(column=2, row=2, sticky=NW, padx=10, pady=15)
-todoFrame.grid_remove()
+resin_timer = ttk.Label(root, text="Resin Timer")
+resin_timer.grid(column=2, row=2, sticky=N)
+resin_timer.grid_remove()
 
-def feat1():
-    labe12.grid_remove()
-    todoFrame.grid_remove()
-    label1.grid()
-    rt.config(bootstyle='primary-outline')
-    chr.config(bootstyle='danger')
-    td.config(bootstyle='primary-outline')
+todoList = todo.ToDoAppFrame(root)
+todoList.grid(column=2, row=2, padx=10, pady=15, sticky=N)
+todoList.grid_remove()
 
-def feat2():
-    label1.grid_remove()
-    todoFrame.grid_remove()
-    labe12.grid()
-    rt.config(bootstyle='danger')
-    chr.config(bootstyle='primary-outline')
-    td.config(bootstyle='primary-outline')
+notifier = ttk.Label(root, text="Notifier")
+notifier.grid(column=2, row=2, padx=10, pady=15, sticky=N)
+notifier.grid_remove()
 
-def todoApp():
-    label1.grid_remove()
-    labe12.grid_remove()
-    todoFrame.grid()
-    td.config(bootstyle='danger')
-    chr.config(bootstyle='primary-outline')
-    rt.config(bootstyle='primary-outline')
+## Side bar functions
+def CharacterSearch_grid():
+    characterSearch.grid()
+    CharacterLevelCalc.grid_remove()
+    weapon.grid_remove()
+    resin_timer.grid_remove()
+    todoList.grid_remove()
+    notifier.grid_remove()
+    
+    characterSearch_button.config(bootstyle='danger', image=characterIconW)
+    characterLevelCalc_button.config(bootstyle='primary-outline', image=calcIconB)
+    weapon_button.config(bootstyle='primary-outline', image=weaponsIconB)
+    resin_button.config(bootstyle='primary-outline')
+    todo_button.config(bootstyle='primary-outline', image=todoIconB)
+    notifier_button.config(bootstyle='primary-outline', image=notificationIconB)
+
+def CharacterLevelCalc_grid():
+    characterSearch.grid_remove()
+    CharacterLevelCalc.grid()
+    weapon.grid_remove()
+    resin_timer.grid_remove()
+    todoList.grid_remove()
+    notifier.grid_remove()
+
+    characterSearch_button.config(bootstyle='primary-outline', image=characterIconB)
+    characterLevelCalc_button.config(bootstyle='danger', image=calcIconW)
+    weapon_button.config(bootstyle='primary-outline', image=weaponsIconB)
+    resin_button.config(bootstyle='primary-outline')
+    todo_button.config(bootstyle='primary-outline', image=todoIconB)
+    notifier_button.config(bootstyle='primary-outline', image=notificationIconB)
+
+def weapon_grid():
+    characterSearch.grid_remove()
+    CharacterLevelCalc.grid_remove()
+    weapon.grid()
+    resin_timer.grid_remove()
+    todoList.grid_remove()
+    notifier.grid_remove()
+
+    characterSearch_button.config(bootstyle='primary-outline', image=characterIconB)
+    characterLevelCalc_button.config(bootstyle='primary-outline', image=calcIconB)
+    weapon_button.config(bootstyle='danger', image=weaponsIconW)
+    resin_button.config(bootstyle='primary-outline')
+    todo_button.config(bootstyle='primary-outline', image=todoIconB)
+    notifier_button.config(bootstyle='primary-outline', image=notificationIconB)
+
+def resin_timer_grid():
+    characterSearch.grid_remove()
+    CharacterLevelCalc.grid_remove()
+    weapon.grid_remove()
+    resin_timer.grid()
+    todoList.grid_remove()
+    notifier.grid_remove()
+
+    characterSearch_button.config(bootstyle='primary-outline', image=characterIconB)
+    characterLevelCalc_button.config(bootstyle='primary-outline', image=calcIconB)
+    weapon_button.config(bootstyle='primary-outline', image=weaponsIconB)
+    resin_button.config(bootstyle='danger')
+    todo_button.config(bootstyle='primary-outline', image=todoIconB)
+    notifier_button.config(bootstyle='primary-outline', image=notificationIconB)
+
+def todo_grid():
+    characterSearch.grid_remove()
+    CharacterLevelCalc.grid_remove()
+    weapon.grid_remove()
+    resin_timer.grid_remove()
+    todoList.grid()
+    notifier.grid_remove()
+
+    characterSearch_button.config(bootstyle='primary-outline', image=characterIconB)
+    characterLevelCalc_button.config(bootstyle='primary-outline', image=calcIconB)
+    weapon_button.config(bootstyle='primary-outline', image=weaponsIconB)
+    resin_button.config(bootstyle='primary-outline')
+    todo_button.config(bootstyle='danger', image=todoIconW)
+    notifier_button.config(bootstyle='primary-outline', image=notificationIconB)
+
+def notifier_grid():
+    characterSearch.grid_remove()
+    CharacterLevelCalc.grid_remove()
+    weapon.grid_remove()
+    resin_timer.grid_remove()
+    todoList.grid_remove()
+    notifier.grid()
+
+    characterSearch_button.config(bootstyle='primary-outline', image=characterIconB)
+    characterLevelCalc_button.config(bootstyle='primary-outline', image=calcIconB)
+    weapon_button.config(bootstyle='primary-outline', image=weaponsIconB)
+    resin_button.config(bootstyle='primary-outline')
+    todo_button.config(bootstyle='primary-outline', image=todoIconB)
+    notifier_button.config(bootstyle='danger', image=notificationIconW)
 
 
-unknown = Image.open("C:\\Users\\Jiahe31\\Downloads\\q2 (1).png")
-unknownImg = ImageTk.PhotoImage(unknown)
-checklist = Image.open("C:\\Users\\Jiahe31\\Downloads\\t1.png")
-checklistImg = ImageTk.PhotoImage(checklist)
 
-chr = ttk.Button(sideBarFrame, text="Chacracter", image=unknownImg, compound=LEFT, width=10, padding=10, command=feat1, bootstyle='danger', takefocus=False)
-chr.grid(column=0, row=1, padx=10, pady=15)
-rt = ttk.Button(sideBarFrame, text="Resin Timer", image=unknownImg, compound=LEFT, width=10, padding=10, command=feat2, bootstyle='primary-outline', takefocus=False)
-rt.grid(column=0, row=2, padx=10, pady=(0, 15))
-td = ttk.Button(sideBarFrame, text="To-Do List", image=checklistImg, compound=LEFT, width=10, padding=10, command=todoApp, bootstyle='primary-outline', takefocus=False)
-td.grid(column=0, row=3, padx=10, pady=(0, 15), sticky=EW)
+## Icon 
+current_path = os.path.abspath(os.getcwd())
+characterImgB = Image.open(current_path + "\Image\Character_icon_black.png")
+characterIconB = ImageTk.PhotoImage(characterImgB)
+characterImgW = Image.open(current_path + "\Image\Character_icon_white.png")
+characterIconW = ImageTk.PhotoImage(characterImgW)
+calcImgB = Image.open(current_path + "\Image\Calc_icon_black.png")
+calcIconB = ImageTk.PhotoImage(calcImgB)
+calcImgW = Image.open(current_path + "\Image\Calc_icon_white.png")
+calcIconW = ImageTk.PhotoImage(calcImgW)
+weaponsImgB = Image.open(current_path + "\Image\Weapons_icon_black.png")
+weaponsIconB = ImageTk.PhotoImage(weaponsImgB)
+weaponsImgW = Image.open(current_path + "\Image\Weapons_icon_white.png")
+weaponsIconW = ImageTk.PhotoImage(weaponsImgW)
+resinImg = Image.open(current_path + "\Image\\resin.png")
+resinIcon = ImageTk.PhotoImage(resinImg)
+todoImgB = Image.open(current_path + "\Image\Todo_icon_black.png")
+todoIconB = ImageTk.PhotoImage(todoImgB)
+todoImgW = Image.open(current_path + "\Image\Todo_icon_white.png")
+todoIconW = ImageTk.PhotoImage(todoImgW)
+notificationImgB = Image.open(current_path + "\Image\\notification_icon_black.png")
+notificationIconB = ImageTk.PhotoImage(notificationImgB)
+notificationImgW = Image.open(current_path + "\Image\\notification_icon_white.png")
+notificationIconW = ImageTk.PhotoImage(notificationImgW)
+
+## Side bar
+characterSearch_button = ttk.Button(sideBarFrame, text="Chacracters", image=characterIconW, compound=LEFT, width=10, padding=10, command=CharacterSearch_grid, bootstyle='danger', takefocus=False)
+characterSearch_button.grid(column=1, row=1, padx=10, pady=15)
+characterLevelCalc_button = ttk.Button(sideBarFrame, text="Character\nCalculator", image=calcIconB, compound=LEFT, width=10, padding=10, command=CharacterLevelCalc_grid, bootstyle='primary-outline', takefocus=False)
+characterLevelCalc_button.grid(column=1, row=2, padx=10, pady=(0, 15))
+weapon_button = ttk.Button(sideBarFrame, text="Weapons", image=weaponsIconB, compound=LEFT, width=10, padding=10, command=weapon_grid, bootstyle='primary-outline', takefocus=False)
+weapon_button.grid(column=1, row=3, padx=10, pady=(0, 15))
+resin_button = ttk.Button(sideBarFrame, text="Resin Timer", image=resinIcon, compound=LEFT, width=10, padding=10, command=resin_timer_grid, bootstyle='primary-outline', takefocus=False)
+resin_button.grid(column=1, row=4, padx=10, pady=(0, 15))
+todo_button = ttk.Button(sideBarFrame, text="To-Do List", image=todoIconB, compound=LEFT, width=10, padding=10, command=todo_grid, bootstyle='primary-outline', takefocus=False)
+todo_button.grid(column=1, row=5, padx=10, pady=(0, 15))
+notifier_button = ttk.Button(sideBarFrame, text="Notification", image=notificationIconB, compound=LEFT, width=10, padding=10, command=notifier_grid, bootstyle='primary-outline', takefocus=False)
+notifier_button.grid(column=1, row=6, padx=10, pady=(0, 15))
 # ttk.Button(sideBarFrame, width=15, padding=13, bootstyle='primary', takefocus=False).grid(column=0, row=4, sticky=W, ipady=200)
 
 sideBarFrame.update()
